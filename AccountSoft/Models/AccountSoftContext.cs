c785f85e43d9d16b062430faea62f28ad9f06ba6
Grethel Mairena <grethelmairena1990@hotmail.com>
2017-07-30 11:45:32 -0600

    first commit

---
+++ AccountSoft/Models/AccountSoftContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AccountSoft.Models
+{
+    public class AccountSoftContext: DbContext
+    {
+        public AccountSoftContext() 
+            : base ("AccountSoftConectionString")
+        {
+        }
+    }
+}

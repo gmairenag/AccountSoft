bea2aa2e734a246b01b5a89fdd6759fe87acd582
Helson Luna <helsonluna@gmail.com>
2017-07-30 12:10:16 -0600

    Correccion cadena de conexion Identity DbContex

--- AccountSoft/Models/IdentityModels.cs
+++ AccountSoft/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace AccountSoft.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AccountSoftConectionString", throwIfV1Schema: false)
         {
         }
 
